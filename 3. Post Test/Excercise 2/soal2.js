8e5bbb6073ca0c7bd10fe67ecba19c54af2196de
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-08-08 10:09:19 +0700

    finish assignment day 5 function lodash

---
+++ 3. Post Test/Excercise 2/soal2.js
@@ -0,0 +1,19 @@
+// Soal:
+// Hilangkan Karakter Berulang yang terdapat pada String
+// Clue : menggunakan perulangan
+// Tidak boleh langsung print expected output
+
+/*
+Expected Output:
+[ 'A', 'B', 'C', 'D', 'F', 'G' ]
+[ 'R', 'O', 'A', 'Q', 'P' ]
+*/
+
+// Jawaban:
+function removeDuplicates (str) {
+    //write your code here
+    
+};
+
+console.log(removeDuplicates("AAAABBBCDFFFDAABBBCG"))
+console.log(removeDuplicates("RRRRROOOAAQPPP"))
