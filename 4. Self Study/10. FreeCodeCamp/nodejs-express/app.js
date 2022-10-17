084a94dfb5bfb7343409e8a3d834de5432b2fdf6
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-10-17 15:45:23 +0700

    add freecodecamp expressjs

---
+++ 4. Self Study/10. FreeCodeCamp/nodejs-express/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { products } = require('./data')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+  const newProduct = products.map((product) => {
+    const { id, name, image } = product
+    return { id, name, image }
+  })
+  res.json({ newProduct })
+})
+
+app.listen(8080, () => {
+  console.log('http://localhost:8080')
+})
