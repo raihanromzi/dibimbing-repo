eac456868754bf03f114fcb1f4f551c2468ce1c3
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-10-17 09:01:49 +0700

    express

---
+++ 4. Self Study/9. NodeJS Udemy/2. ExpressJS/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+
+app.use((req, res, next) => {
+  console.log('Hello')
+})
+
+app.use((req, res, next) => {
+  console.log('Bitch')
+  res.send('Hello')
+  next()
+})
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World')
+})
+
+app.listen(8080)
